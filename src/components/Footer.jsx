1a584a4b6f8aa8c86daaffc5c7f7f942d4596342
Đurđica Jovanović <jovanovic.djurdjica@hmail.com>
2023-02-09 12:38:30 +0100

    dodavanje digitalnog sata

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 
 function Footer() {
-  return (
-    <div className='footer'>
-        <h3>Đurđica Jovanović 2019/446 </h3>
-    </div>
-  )
+	//dodavanje sata
+	const [time, setTime] = useState("");
+	function formatTime(val) {
+		if (val < 10) {
+			return 0;
+		} else {
+			return "";
+		}
+	}
+
+	useEffect(() => {
+		const timerID = setInterval(() => tick(), 1000);
+
+		return function cleanup() {
+			clearInterval(timerID);
+		};
+	});
+
+	function tick() {
+		const d = new Date();
+		const h = d.getHours();
+		const m = d.getMinutes();
+		const s = d.getSeconds();
+
+		setTime(
+			formatTime(h) + h + ":" + formatTime(m) + m + ":" + formatTime(s) + s
+		);
+	}
+	return (
+		<div className="footer">
+			<h1> {time}</h1>
+		</div>
+	);
 }
 
-export default Footer
+export default Footer;
